6f8d741a6a6132b8fc7bc134c62ad2f126675fbf
NarendraReddy <narendra.nya@gmail.com>
2024-05-07 18:36:03 -0700

    TF3

---
+++ expense/variables.tf
@@ -0,0 +1,54 @@
+variable "image_id" {
+    type= string
+    default = "ami-090252cbe067a9e58"
+    description= "RHEL9 AMI ID"
+ 
+}
+
+variable "instance_type" {
+    type=string
+    default = "t3.micro"  
+}
+
+variable "common_tags" {
+    default = {
+        project="Expense"
+        Environment="Dev"
+        Terraform = "true"
+        # Module="DB"
+        # Name="DB" #same name displayed
+    }
+  
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+  
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+  
+}
+
+variable "ssh_port" {
+    type=number
+    default = 22
+  
+}
+
+variable "protocol" {
+    default = "tcp"
+  
+}
+
+variable "allowed_cidr" {
+    type=list(string)
+    default =  ["0.0.0.0/0"]  
+}
+
+variable "instance_names" {
+    type=list 
+    default = ["db","backend","frontend"]
+  
+}
